271016a72fb1cbc971cc192ab1813c08b9770896
zakaria abdessamed brahimi <brahimi.zakaria.abdessamed@gmail.com>
2020-11-20 15:44:34 +0100

    Adding OOP.py file

---
+++ Notes.md
@@ -0,0 +1,10 @@
+- Everything in Python actually is Objects
+- when we create something in python we really create an object which is an instance of specific class and that class define the way that object interact with other things in programme
+
+- creating new instance ===>> f = dog() ===>> new instance(object)
+- Attribute ===>> self.aaaaa = name
+
+----- Object Oriented three pillars:
+                                    - Encapsulation = التغليق ===>> using "self" argument
+                                    - Inheritance   = الوراثة
+                                    - Polymorphisme = 
--- OOP.py
+++ OOP.py
@@ -1,1 +1,35 @@
-gggg
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def get_grade(self):
+        return self.grade
+
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+
+s1 = Student("zaki", 20, 99)
+s2 = Student("abdou", 23, 50)
+s3 = Student("ayoube", 14, 100)
+
+course = Course('Science', 2)
+course.add_student(s1)
+course.add_student(s2)
+# students = [("zaki", 20, 99), ("abdou", 23, 50)]
+print(course.students[1].age)
+
+# if __name__ == "__main__":
+#     x = 5
+    # help(x)
+
