533d6c029f565614f21682a09219c7d52bc3f7c3
zakaria abdessamed brahimi <brahimi.zakaria.abdessamed@gmail.com>
2020-11-21 19:19:00 +0100

    Adding Bank Class

--- OOP.py
+++ OOP.py
@@ -1,46 +1,40 @@
-# class Student:
-#     def __init__(self, name, age, grade):
-#         self.name = name
-#         self.age = age
-#         self.grade = grade
-
-#     def get_grade(self):
-#         return self.grade
-
-# class Course:
-#     def __init__(self, name, max_students):
-#         self.name = name
-#         self.max_students = max_students
-#         self.students = []
-
-#     def add_student(self, student):
-#         if len(self.students) < self.max_students:
-#             self.students.append(student)
-#             return True
-#         return False
-
-# s1 = Student("zaki", 20, 99)
-# s2 = Student("abdou", 23, 50)
-# s3 = Student("ayoube", 14, 100)
-
-# course = Course('Science', 2)
-# course.add_student(s1)
-# course.add_student(s2)
-# # students = [("zaki", 20, 99), ("abdou", 23, 50)]
-# print(course.students[1].age)
-
-# # if __name__ == "__main__":
-# #     x = 5
-#     # help(x)
-
-
-class test:
-    def __init__(self, name, age):
+class User:
+    def __init__(self, name, age, gender):
         self.name = name
-        self.age  = age
+        self.age = age
+        self.gender = gender
 
-    def test_method(self, grade):
-        self.grade = grade
-    
-t1 = test("zaki", 20)
-print(t1.test_method(50).grade)
+    def show_detials(self):
+        print('User Detials:')
+        print("")
+        print(f'name : {self.name}')
+        print(f'age : {self.age}')
+        print(f'gender : {self.gender}')
+
+class Bank(User):
+    def __init__(self, name, age, gender):
+        super().__init__(name, age, gender)
+        self.balance = 0
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print(f'Account balace has been updated : ${self.balance}')
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print(f'Insuficiant funds | Balance availible : ${self.balance}')
+        else:
+            self.balance = self.balance - amount
+            print(f'Account balance has been updated : ${self.balance}')
+
+    def view_balance(self):
+        self.show_detials()
+        print(f'Account balance : ${self.balance}')
+
+
+
+b1 = Bank('zaki', 20, 'male')
+b1.show_detials()
+b1.deposit(100)
+b1.withdraw(50)
+b1.view_balance()
